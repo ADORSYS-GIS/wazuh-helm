8726e172daeff9e87d680fe98cbbc9aa7190c275
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-11-19 04:40:01 +0100

    feat: writing the cnf file

--- wazuh/files/scripts/gen_certs.sh
+++ wazuh/files/scripts/gen_certs.sh
@@ -41,47 +41,33 @@ generate_cert() {
   # Generate a private key
   openssl genrsa -out "$OUTPUT_FOLDER/${CONTEXT}-key.pem" 2048
 
-  # Create an OpenSSL configuration file for SAN
-  CERT_CONFIG="$OUTPUT_FOLDER/openssl-san.${CONTEXT}.cnf"
-  cat > "$CERT_CONFIG" <<EOL
-[req]
-default_bits = 2048
-x509_extensions = v3_ca
-prompt = no
-default_md = sha256
-distinguished_name = dn
-req_extensions = req_ext
+  # Generate the CSR and certificate directly with SANs
+  openssl req -new -key "$OUTPUT_FOLDER/${CONTEXT}-key.pem" \
+    -subj "/C=DE/L=Bayern/O=Adorsys/CN=${DOMAINS_ARRAY[0]}" \
+    -out "$OUTPUT_FOLDER/${CONTEXT}.csr"
 
-[dn]
-C = DE
-L = Bayern
-O = Adorsys
-CN = ${DOMAINS_ARRAY[0]}
-
-[req_ext]
-subjectAltName = @alt_names
-
-[alt_names]
-EOL
-
-  # Append SAN entries
-  for i in "${!DOMAINS_ARRAY[@]}"; do
-    echo "DNS.$((i + 1)) = ${DOMAINS_ARRAY[$i]}" >> "$CERT_CONFIG"
-  done
-
-  # Generate the CSR
-  openssl req -new -days 3650 -key "$OUTPUT_FOLDER/${CONTEXT}-key.pem" -out "$OUTPUT_FOLDER/${CONTEXT}.csr" -config "$CERT_CONFIG"
-
-  # Sign the CSR
   openssl x509 -req -days 3650 \
     -in "$OUTPUT_FOLDER/${CONTEXT}.csr" \
     -CA "$OUTPUT_FOLDER/root-ca.pem" \
     -CAkey "$OUTPUT_FOLDER/root-ca-key.pem" \
     -CAcreateserial -sha256 \
-    -out "$OUTPUT_FOLDER/${CONTEXT}.pem"
+    -out "$OUTPUT_FOLDER/${CONTEXT}.pem" \
+    -extfile <(
+      cat <<EOL
+[req_ext]
+subjectAltName = @alt_names
+
+[alt_names]
+EOL
+      for i in "${!DOMAINS_ARRAY[@]}"; do
+        echo "DNS.$((i + 1)) = ${DOMAINS_ARRAY[$i]}"
+      done
+    )
 
   echo "Certificate for '$OUTPUT_FOLDER/${CONTEXT}' created: '$OUTPUT_FOLDER/${CONTEXT}.pem'"
 
   rm "$OUTPUT_FOLDER/${CONTEXT}.csr"
-  rm "$CERT_CONFIG"
 }
+
+# Example usage: Replace with your context and domain files or strings
+# Example: generate_cert "node" "wazuh-indexer,*.wazuh-indexer,wazuh-indexer-api,*.wazuh-indexer-api"
